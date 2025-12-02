c4a558cd69a1ef8e1a11652d30fb51716c27a099
StellinoX <mate86578@gmail.com>
2025-12-02 17:06:04 +0100

    Initial Commit

---
+++ app ch3/app_ch3App.swift
@@ -0,0 +1,17 @@
+//
+//  app_ch3App.swift
+//  app ch3
+//
+//  Created by Alfonso Giuseppe Auriemma on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct app_ch3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
